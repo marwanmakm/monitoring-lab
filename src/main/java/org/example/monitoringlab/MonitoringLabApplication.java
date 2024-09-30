3a602d11c0c19c6aefa74def88293621864ab964
Marwan Makarem <marwanmakarem16@gmail.com>
2024-09-29 23:01:46 -0300

    INITIAL COMMIT

---
+++ src/main/java/org/example/monitoringlab/MonitoringLabApplication.java
@@ -0,0 +1,12 @@
+package org.example.monitoringlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringLabApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MonitoringLabApplication.class, args);
+  }
+}
